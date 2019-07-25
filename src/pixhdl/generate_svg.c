66ac2a81d067e25e4e5dbb6c4368636b7fe9521f
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-07-25 11:29:23 +0200

    Start generating hardcoded svgs

--- src/pixhdl/generate_svg.c
+++ src/pixhdl/generate_svg.c
@@ -1,1 +1,24 @@
 #include "generate_svg.h"
+
+
+#define SVG_STRING "<svg version=\"1.1\"\
+     baseProfile=\"full\"\
+     width=\"300\" height=\"200\"\
+     xmlns=\"http://www.w3.org/2000/svg\">\
+  <rect width=\"100%\" height=\"100%\" fill=\"red\" />\
+  <circle cx=\"150\" cy=\"100\" r=\"80\" fill=\"green\" />\
+  <text x=\"150\" y=\"125\" font-size=\"60\" text-anchor=\"middle\" fill=\"white\">SVG</text>\
+</svg>"
+
+int generateSvgFromEntity (Entity * ent, char * filename)
+{
+    FILE * svg_file = NULL;
+
+    svg_file = fopen(filename, "w");
+
+    fprintf(svg_file, "%s\n", SVG_STRING);
+
+    fclose(svg_file);
+
+    return 0;
+}
