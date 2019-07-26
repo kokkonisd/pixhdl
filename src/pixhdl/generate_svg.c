b9bb23efa70b79e8b04f32a93299c29f86e73011
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-07-26 17:24:05 +0200

    Get started on generating entity rect SVG

--- src/pixhdl/generate_svg.c
+++ src/pixhdl/generate_svg.c
@@ -1,22 +1,49 @@
 #include "generate_svg.h"
 
 
-#define SVG_STRING "<svg version=\"1.1\"\
-     baseProfile=\"full\"\
-     width=\"300\" height=\"200\"\
-     xmlns=\"http://www.w3.org/2000/svg\">\
-  <rect width=\"100%\" height=\"100%\" fill=\"red\" />\
-  <circle cx=\"150\" cy=\"100\" r=\"80\" fill=\"green\" />\
-  <text x=\"150\" y=\"125\" font-size=\"60\" text-anchor=\"middle\" fill=\"white\">SVG</text>\
-</svg>"
+#define TST_MSG "I say: %s\n"
+
+
+int max (int a, int b)
+{
+    return a > b ? a : b;
+}
+
 
 int generateSvgFromEntity (Entity * ent, char * filename)
 {
     FILE * svg_file = NULL;
+    int svg_width = 0;
+    int svg_height = 0;
+    int rect_width = 0;
+    int rect_height = 0;
+    int port_width = 0;
+    int port_height = 0;
+
+    port_width = ARROW_LENGTH + (ent->max_name_size + 1) * PORT_NAME_FONT_SIZE;
+    port_height = ARROW_WIDTH;
+
+    rect_width = max(ENTITY_NAME_FONT_SIZE * (strlen(ent->name) + 2), ARROW_WIDTH * ent->count_inout);
+    rect_height = max(max(ent->count_in, ent->count_out) * ARROW_WIDTH, ENTITY_NAME_FONT_SIZE * (strlen(ent->name) + 2));
+
+    svg_width = rect_width + port_width * 2;
+
+    svg_height = rect_height + RECTANGLE_STROKE_WIDTH;
+    if (ent->count_inout)
+        svg_height += ARROW_WIDTH + (PORT_NAME_FONT_SIZE * (ent->max_name_size + 2));
+
+    printf("%d %d\n", svg_width, svg_height);
 
     svg_file = fopen(filename, "w");
 
-    fprintf(svg_file, "%s\n", SVG_STRING);
+    fprintf(svg_file, SVG_HEADER, svg_width, svg_height);
+    fprintf(svg_file, "<rect width='100%%' height='100%%' fill='red' />\n");
+    fprintf(svg_file, "<rect width='%d' height='%d' x='%d' y='%d' fill='none' stroke='black' stroke-width='%d' />\n",
+                      rect_width, rect_height, port_width, RECTANGLE_STROKE_WIDTH / 2, RECTANGLE_STROKE_WIDTH);
+    fprintf(svg_file, "<text text-anchor='middle' x='%d' y='%d' font-size='%d' fill='white'>%s</text>\n",
+                      port_width + rect_width / 2, rect_height / 2 + RECTANGLE_STROKE_WIDTH / 2,
+                      ENTITY_NAME_FONT_SIZE, ent->name);
+    fprintf(svg_file, "</svg>\n");
 
     fclose(svg_file);
 
--- src/pixhdl/generate_svg.h
+++ src/pixhdl/generate_svg.h
@@ -1,6 +1,13 @@
 #ifndef __GENERATE_SVG_H__
 #define __GENERATE_SVG_H__
 
+#define ENTITY_NAME_FONT_SIZE 60
+#define RECTANGLE_STROKE_WIDTH 10
+#define ARROW_WIDTH 50
+#define ARROW_LENGTH 200
+#define PORT_NAME_FONT_SIZE (ENTITY_NAME_FONT_SIZE - 4)
+
+#define SVG_HEADER "<svg version='1.1' baseProfile='full' width='%d' height='%d' xmlns='http://www.w3.org/2000/svg'>\n"
 
 #include "entity.h"
 #include "parser.h"
