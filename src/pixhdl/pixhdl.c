0105201a590f29f052f750d0e379a70d419a386a
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-07-29 10:29:29 +0200

    Add -o flag functionality

--- src/pixhdl/pixhdl.c
+++ src/pixhdl/pixhdl.c
@@ -18,12 +18,15 @@ int main (int argc, char * argv[])
     int ra = 0;
     Entity * ent = NULL;
     int i = 0;
+    char * out_filename = NULL;
+    char * copy_ptr = NULL;
+    int j = 0;
 
     // If no arguments are given, print the help screen
     if (argc == 1) return print_help_screen();
 
     // Parse optional arguments
-    while ((ra = getopt(argc, argv, ":vh")) != -1) {
+    while ((ra = getopt(argc, argv, ":vho:")) != -1) {
         switch (ra) {
             case 'v':
                 // Print version number
@@ -35,10 +38,21 @@ int main (int argc, char * argv[])
                 print_help_screen();
                 return 0;
 
+            case 'o':
+                out_filename = optarg;
+                break;
+
             case '?':
             default:
+                // Show error for -o flag without an argument
+                if (optopt == 'o') {
+                    log_err("Option '-o' requires an argument.");
+                    return 1;
+                }
+                
                 // Unknown optional argument
                 log_err("Unknown argument `%s`. Use `pixhdl -h` to get a complete list of valid arguments.", argv[optind - 1]);
+                return 1;
         }
     }
 
@@ -48,9 +62,35 @@ int main (int argc, char * argv[])
         ent = getEntityFromFile(argv[i]);
         check(ent, "Couldn't get entity from file `%s`.", argv[i]);
 
-        // Print the entity's signals
-        // printEntitySignals(ent);
-        generateSvgFromEntity(ent, "test.svg");
+        if (!out_filename) {
+            // Allocate memory for the name of the output file
+            out_filename = malloc(sizeof(char) * (strlen(argv[i]) + 5));
+
+            // Point the copy pointer at the start of the input file string
+            copy_ptr = argv[i];
+            // Initialize the char counter at 0
+            j = 0;
+
+            // Copy the whole filename except the .* ending
+            while (*copy_ptr != '.')
+                out_filename[j++] = *copy_ptr++;
+
+            // Write '.svg' at the end of the filename
+            out_filename[j] = '.';
+            out_filename[j + 1] = 's';
+            out_filename[j + 2] = 'v';
+            out_filename[j + 3] = 'g';
+            out_filename[j + 4] = '\0';
+
+            // Generate an SVG
+            generateSvgFromEntity(ent, out_filename);
+
+            // Free the generated name
+            free(out_filename);
+        } else {
+            // Generate an SVG
+            generateSvgFromEntity(ent, out_filename);
+        }
 
         // Destroy the entity object
         destroyEntity(ent);
