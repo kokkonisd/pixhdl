c8bdfb0d4bea2638b6817fc943d15fc2c1e8a196
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-07-29 13:06:23 +0200

    Add sudos to make uninstall and make install for linux users

--- src/pixhdl/auto_update.h
+++ src/pixhdl/auto_update.h
@@ -9,7 +9,7 @@
 #define VERSION_WIDTH 6
 #define LATEST_VERSION "curl -s https://github.com/kokkonisd/pixhdl/releases/latest | grep -Eo \"/tag/(.+)>\" | head -c 11 | tail -c 5"
 
-#define UPDATE_COMMAND "TMPDWN=$(curl -s https://api.github.com/repos/kokkonisd/pixhdl/releases/latest | grep \"tag_name\" | awk '{print \"https://github.com/kokkonisd/pixhdl/archive/\" substr($2, 2, length($2)-3) \"/pixhdl-\" substr($2, 3, length($2)-4) \".tar.gz\"}') && curl -L -s $TMPDWN -o /tmp/pixhdl_latest.tar.gz && tar xvzf /tmp/pixhdl_latest.tar.gz -C /tmp/ && cd /tmp/pixhdl-* && make clean && make uninstall && make && make install && cd .. && rm -rf pixhdl-* pixhdl_latest*"
+#define UPDATE_COMMAND "TMPDWN=$(curl -s https://api.github.com/repos/kokkonisd/pixhdl/releases/latest | grep \"tag_name\" | awk '{print \"https://github.com/kokkonisd/pixhdl/archive/\" substr($2, 2, length($2)-3) \"/pixhdl-\" substr($2, 3, length($2)-4) \".tar.gz\"}') && curl -L -s $TMPDWN -o /tmp/pixhdl_latest.tar.gz && tar xvzf /tmp/pixhdl_latest.tar.gz -C /tmp/ && cd /tmp/pixhdl-* && make clean && sudo make uninstall && make && sudo make install && cd .. && rm -rf pixhdl-* pixhdl_latest*"
 
 
 int update ();
